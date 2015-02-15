87ee84687ff270383668f1fab7423c9d5a9fef29
DChang87 <diana.chang.twn@gmail.com>
2015-02-15 07:51:20 -0500

    everything but reading+writing to file(s)

--- src/TasksClass.java
+++ src/TasksClass.java
@@ -11,6 +11,8 @@ class TasksClass {
 	public final int EDIT=0,DONE=1;
 	public ArrayList<Integer>editingdetails = new ArrayList<Integer>();
 	public ArrayList<Integer>hoveringText = new ArrayList<Integer>();
+	public int hovering_editDetails=0;
+	public int detailsEditDone=EDIT;
 	public ArrayList<Integer>edit_done = new ArrayList<Integer>();
 	public ArrayList<Integer> hovering=new ArrayList<Integer>();
 	public TasksClass(DrawClass d){
@@ -19,10 +21,52 @@ class TasksClass {
 	public void readFile(){
 		String name=drawPanel.currentdate[2]+"-"+drawPanel.currentdate[1]+"-"+drawPanel.currentdate[0]+".txt";
 	}
+	public void checkEditDonedetails(Organizer o){
+		if (o.down && drawPanel.setD.collide(220, 530,50,28,o.mouseX, o.mouseY)){
+			if (detailsEditDone==EDIT){
+				detailsEditDone=DONE;
+				showTextFields();
+			}
+			else{
+				detailsEditDone=EDIT;
+				hideTextFields();
+				updateTextFields();
+			}
+			o.down=false;
+		}
+		else if (drawPanel.setD.collide(220,530,50,28, o.mouseX, o.mouseY)){
+			hovering_editDetails=2;
+		}
+		else{
+			hovering_editDetails=0;
+		}
+	}
+	public void showTextFields(){
+		for (int i=0;i<drawPanel.org.detailtextfields.size();i++){
+			drawPanel.org.detailtextfields.get(i).setVisible(true);
+		}
+		drawPanel.org.notesArea.setVisible(true);
+	}
+	public void hideTextFields(){
+		for (int i=0;i<drawPanel.org.detailtextfields.size();i++){
+			drawPanel.org.detailtextfields.get(i).setVisible(false);
+		}
+		drawPanel.org.notesArea.setVisible(false);
+	}
+	public void updateTextFields(){
+		allTasks.set(currentTask, drawPanel.org.detailtextfields.get(0).getText());
+		rank.set(currentTask, Integer.parseInt(drawPanel.org.detailtextfields.get(1).getText()));
+		completion.set(currentTask,Integer.parseInt(drawPanel.org.detailtextfields.get(2).getText()));
+		dueDates.get(currentTask).set(0,Integer.parseInt(drawPanel.org.detailtextfields.get(3).getText()));
+		dueDates.get(currentTask).set(1,Integer.parseInt(drawPanel.org.detailtextfields.get(4).getText()));
+		dueDates.get(currentTask).set(2,Integer.parseInt(drawPanel.org.detailtextfields.get(5).getText()));
+		notes.set(currentTask, drawPanel.org.notesArea.getText());
+	}
 	public void checkNTCollision(Organizer o){
 		if (o.down&&drawPanel.setD.collide(480, 515, 150, 50, o.mouseX, o.mouseY)){
 			newTask();
 			o.down=false;
+			detailsEditDone=DONE;
 		}
 		else if (drawPanel.setD.collide(480, 515, 150, 50, o.mouseX,o.mouseY)){
 			drawPanel.newtaskDraw=drawPanel.newtaskbutton2;
@@ -67,10 +111,7 @@ class TasksClass {
 				edit_done.set(i,EDIT);
 			}
 		}
-		for (int i=0;i<drawPanel.org.detailtextfields.size();i++){
-			drawPanel.org.detailtextfields.get(i).setVisible(true);
-		}
-		drawPanel.org.notesArea.setVisible(true);
+		showTextFields();
 		edit_done.add(DONE);
 		rank.add(0);
 		notes.add("");
@@ -82,24 +123,17 @@ class TasksClass {
 		dueDates.get(currentTask).add(drawPanel.currentdate[0]);
 		dueDates.get(currentTask).add(drawPanel.currentdate[1]);
 		dueDates.get(currentTask).add(drawPanel.currentdate[2]);
-		
 	}
 	public void checkEDCollide(Organizer o,int i){
 		if (o.down && drawPanel.setD.collide(730, 160+i*40, 50, 28, o.mouseX, o.mouseY)){
 			if (edit_done.get(i)==EDIT){
-				/*for (int k=0;k<drawPanel.org.tasklist.size();k++){
-					drawPanel.org.tasklist.get(i).setVisible(false);
-					edit_done.set(i,EDIT);
-					allTasks.set(i,drawPanel.org.tasklist.get(i).getText());
-					System.out.println("invisible");
-				}*/
 				edit_done.set(i,DONE);
 				drawPanel.org.tasklist.get(i).setVisible(true);
-				System.out.println("edit");
 			}
 			else{
 				edit_done.set(i,EDIT);
 				allTasks.set(i, drawPanel.org.tasklist.get(i).getText());
+				drawPanel.org.detailtextfields.get(0).setText(drawPanel.org.tasklist.get(i).getText());
 				drawPanel.org.tasklist.get(i).setVisible(false);
 			}
 			o.down=false;
