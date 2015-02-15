943f0f479838b002a1d59482ded1e44f4b4eea8a
DChang87 <diana.chang.twn@gmail.com>
2015-02-15 02:12:47 -0500

    Can edit task name

--- src/TasksClass.java
+++ src/TasksClass.java
@@ -8,7 +8,9 @@ class TasksClass {
 	public ArrayList<Integer> rank=new ArrayList<Integer>();
 	public ArrayList<String> notes = new ArrayList<String>();
 	public int currentTask=0;
-	//public ArrayList<boolean>edit_done = new ArrayList<boolean>();
+	public final int EDIT=0,DONE=1;
+	public ArrayList<Integer>edit_done = new ArrayList<Integer>();
+	public ArrayList<Integer> hovering=new ArrayList<Integer>();
 	public TasksClass(DrawClass d){
 		drawPanel=d;
 	}
@@ -48,17 +50,36 @@ class TasksClass {
 			}
 			allTasks.set(i,drawPanel.org.tasklist.get(i).getText());
 		}
-		System.out.println(allTasks.toString());
+		edit_done.add(DONE);
 		rank.add(0);
 		notes.add("");
 		completion.add(0);
-		
+		hovering.add(0);
 		System.out.println("newtask");
 		dueDates.add(new ArrayList<Integer>());
 		dueDates.get(currentTask).add(drawPanel.currentdate[0]);
 		dueDates.get(currentTask).add(drawPanel.currentdate[1]);
 		dueDates.get(currentTask).add(drawPanel.currentdate[2]);
 		
+	}
+	public void checkEDCollide(Organizer o,int i){
+		if (o.down && drawPanel.setD.collide(730, 160+i*40, 50, 28, o.mouseX, o.mouseY)){
+			if (edit_done.get(i)==EDIT){
+				edit_done.set(i,DONE);
+				drawPanel.org.tasklist.get(i).setVisible(true);
+			}
+			else{
+				edit_done.set(i,EDIT);
+				allTasks.set(i, drawPanel.org.tasklist.get(i).getText());
+				drawPanel.org.tasklist.get(i).setVisible(false);
+			}
+		}
+		else if (drawPanel.setD.collide(730, 160+i*40, 50, 28, o.mouseX, o.mouseY)){
+			hovering.set(i,2);
+		}
+		else{
+			hovering.set(i,0);
+		}
 	}
 	public void clearDay(){
 		for (int i=0;i<drawPanel.org.tasklist.size();i++){
@@ -67,6 +88,7 @@ class TasksClass {
 			drawPanel.repaint();
 		}
 		drawPanel.org.tasklist.clear();
+		edit_done.clear();
 		numTasks=0;
 		allTasks.clear();
 		rank.clear();
